be9e8326a9ffc9d971e4ab2c4491fc8761cfe742
Musgro9 <kmusgrove2289@gmail.com>
2022-05-02 17:04:39 -0400

    commit

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ const AuthFlow = () => {
     <AuthStack.Navigator>
       <AuthStack.Screen
         name="Signup"
-        component={ProfileScreen}
+        component={SignupScreen}
         options={{ headerShown: false }}
       />
       <AuthStack.Screen
